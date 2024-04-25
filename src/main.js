52c12a465150ae0bfc6c3d2c7add0321f34c549a
kakoyo <kakoyo@qq.com>
2024-04-26 03:04:27 +0800

    axios的基础封装

--- src/main.js
+++ src/main.js
@@ -11,6 +11,12 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import '@/styles/element/index.scss'
 
+// 测试接口函数
+import { getCategory } from '@/apis/testApi'
+
+getCategory().then(res=>{
+    console.log(res);
+})
 const app = createApp(App)
 
 app.use(createPinia())
