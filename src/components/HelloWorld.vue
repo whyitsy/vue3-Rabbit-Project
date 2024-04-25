e15934f666e4718d84e65987211f4f82d019d022
kakoyo <kakoyo@qq.com>
2024-04-26 01:58:22 +0800

    add Element-plus

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -16,6 +16,7 @@ defineProps({
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
     </h3>
   </div>
+  <el-button type="success">Success</el-button>
 </template>
 
 <style scoped>
