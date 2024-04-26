1d167fd104ccce9d1798006d62624799aecb1a4d
kakoyo <kakoyo@qq.com>
2024-04-27 00:01:13 +0800

    home-抽象面板布局,使用props和slot

---
+++ src/views/Home/components/HomePanel.vue
@@ -0,0 +1,55 @@
+<script setup>
+// 有些组件的布局差不多，就将布局抽象出来，使用props或slot来传递变化的地方。
+// props 用来接收简单类型数据; slot 用来接收复杂模板数据
+
+// 定义props, 用来接受简单类型数据
+defineProps({
+    title: String,
+    subTitle: String,
+})
+
+
+</script>
+
+
+<template>
+  <div class="home-panel">
+    <div class="container">
+      <div class="head">
+         <!-- 主标题和副标题 -->
+        <h3>
+          {{title}}<small>{{subTitle}}</small>
+        </h3>
+      </div>
+      <!-- 主体内容区域 -->
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped lang='scss'>
+.home-panel {
+  background-color: #fff;
+
+  .head {
+    padding: 40px 0;
+    display: flex;
+    align-items: flex-end;
+
+    h3 {
+      flex: 1;
+      font-size: 32px;
+      font-weight: normal;
+      margin-left: 6px;
+      height: 35px;
+      line-height: 35px;
+
+      small {
+        font-size: 16px;
+        color: #999;
+        margin-left: 20px;
+      }
+    }
+  }
+}
+</style>
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -3,7 +3,8 @@ import HomeCategory from './components/HomeCategory.vue'
 import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
-import homeProduct from './components/HomeProduct.vue'
+import HomeProduct from './components/HomeProduct.vue'
+import HomePanel from './components/HomePanel.vue'
 </script>
 
 <template>
@@ -13,5 +14,17 @@ import homeProduct from './components/HomeProduct.vue'
   </div>
   <HomeNew />
   <HomeHot />
-  <homeProduct />
+  <HomeProduct />
+
+  <HomePanel title="新鲜好物" sub-Title="新鲜好物2">
+    <div>
+      新鲜好物的内容
+    </div>
+  </HomePanel>
+  
+  <HomePanel title="人气好物" sub-Title="人气好物2">
+    <div>
+      人气好物的内容
+    </div>
+  </HomePanel>
 </template>
