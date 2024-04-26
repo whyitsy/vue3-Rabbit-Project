0264e7810b253e16261de1add813ee5549ecf3c3
kakoyo <kakoyo@qq.com>
2024-04-26 17:10:59 +0800

    引入静态资源文件

--- src/main.js
+++ src/main.js
@@ -9,6 +9,9 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import '@/styles/element/index.scss'
 
+// 引入自定义初始化样式
+import '@/styles/common.scss'
+
 // // 测试接口函数
 // import { getCategory } from '@/apis/testApi'
 
