975b1cb47e2bdf639e1492e65663c0af7a9d9526
kakoyo <kakoyo@qq.com>
2024-04-26 23:20:40 +0800

    home-整体部分拆分以及分类渲染

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,13 +1,17 @@
 <script setup>
-
+import HomeCategory from './components/HomeCategory.vue'
+import HomeBanner from './components/HomeBanner.vue'
+import HomeNew from './components/HomeNew.vue'
+import HomeHot from './components/HomeHot.vue'
+import homeProduct from './components/HomeProduct.vue'
 </script>
 
 <template>
-<div style="height : 1200px">我是Home二级子页面</div>
-
+  <div class="container">
+    <HomeCategory />
+    <HomeBanner />
+  </div>
+  <HomeNew />
+  <HomeHot />
+  <homeProduct />
 </template>
-
-<style scoped>
-
-
-</style>
