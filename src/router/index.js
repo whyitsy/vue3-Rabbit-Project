eff8c32b3fa42fd0403d307b207a82ac0e99ec24
kakoyo <kakoyo@qq.com>
2024-05-01 00:00:19 +0800

    详情-路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -26,6 +26,12 @@ const router = createRouter({
           path: 'category/sub/:id',
           name: 'home.subCategory',
           component: () => import("@/views/SubCategory/index.vue"),
+        },
+        {
+          // 单个商品的详情页路由
+          path: 'detail/:id',
+          name: 'home.detail',
+          component: () => import("@/views/Detail/index.vue")
         }
       ]
     },
@@ -36,10 +42,10 @@ const router = createRouter({
     }
 
   ],
-  scrollBehavior(){
+  scrollBehavior() {
     return {
-      // 路由切换的时候切换到页面顶部
-      top:0
+      // 路由切换时回到顶部
+      top: 0
     }
   }
 })
